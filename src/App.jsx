859c4db2c9570927f3ce11703c89ce888d1776ae
Briuwu <millontebrian@gmail.com>
2023-03-23 18:02:17 +0800

    add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Notification from "./components/Notification";
+
+function App() {
+  return (
+    <main className="grid min-h-screen place-items-center bg-blue-very-light-grayish">
+      <Notification />
+    </main>
+  );
+}
+
+export default App;
